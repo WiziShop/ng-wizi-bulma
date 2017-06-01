db47ad2e93ec944f76cc767b3514f104cdf462bc
numero_2 <alexis@wizishop.com>
2017-06-01 12:04:51 +0200

    add fake-dialog-component.ts

---
+++ src/demo-app/demo-app/fake-dialog-component.ts
@@ -0,0 +1,34 @@
+import {Component, ElementRef, ViewChild} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  providers: [],
+  template: `
+    <div class="field">
+      <label class="label">Name</label>
+      <p class="control">
+        <textarea #myInput class="textarea" placeholder="Name"></textarea>
+      </p>
+      <label class="label">Address</label>
+      <p class="control">
+        <textarea class="textarea" placeholder="Address"></textarea>
+      </p>
+      <label class="label">Other</label>
+      <p class="control">
+        <textarea class="textarea" placeholder="Other"></textarea>
+      </p>
+    </div>
+  `,
+})
+export class FakeDialogComponent {
+
+  @ViewChild('myInput') myInput: ElementRef;
+
+  ngOnInit() {
+    console.log('FakeDialogComponent');
+
+    console.log('myInput', this.myInput);
+  }
+
+
+}
