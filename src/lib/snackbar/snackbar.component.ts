a3247bd39903f274b8bf2ac75d8fae39546a5af5
Jean-Baptiste Malatrasi <jb@wizishop.com>
2017-09-04 11:54:06 +0200

    Use external scss and html

--- src/lib/snackbar/snackbar.component.ts
+++ src/lib/snackbar/snackbar.component.ts
@@ -1,66 +1,17 @@
 import {Observable} from 'rxjs/Observable';
 import {Subject} from 'rxjs/Subject';
 import {NwbSnackbarConfig} from './snackbar.service';
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {animate, AnimationEvent, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'nwb-snack-bar',
-  template: `
-    <div [class]="'notification is-active ' + ((config.color) ? config.color : 'is-transparent')"
-         [@state]="open ? 'active': 'inactive'"
-         (@state.done)="animationDone($event)"
-    >
-      <div class="columns">
-        <div class="column notification--message" [ngClass]="{'is-half': config.action}"
-             [innerHTML]="config.message">
-        </div>
-        <div class="column is-half" *ngIf="config.action">
-          <button [class]="'button ' + ((config.buttonColor) ? config.buttonColor : 'is-success')"
-                  (click)="dismiss(true)">{{config.action}}
-          </button>
-
-        </div>
-      </div>
-    </div>
-  `,
-  styles: [`
-    .notification {
-      position: fixed;
-      bottom: 0;
-      width: 600px;
-      color: #fff;
-      font-size: 15px;
-      font-weight: 300;
-      margin: 0 auto;
-      left: 0;
-      right: 0;
-      z-index: 10;
-    }
-
-    .notification.is-transparent {
-      background-color: rgba(0, 0, 0, 0.8);
-    }
-
-    .notification button {
-      word-wrap: break-word;
-      word-break: break-all;
-      white-space: normal;
-      height: 100%;
-    }
-
-    .notification .column:nth-child(2) {
-      text-align: right;
-    }
-
-    .notification .column:nth-child(1) {
-      text-align: left;
-    }
-
-    .notification--message {
-      align-self: center;
-    }
-  `],
+  templateUrl: './snackbar.component.html',
+  styleUrls: ['./snackbar.component.scss'],
+  host: {
+    'class': 'nwb-snack-bar',
+  },
+  encapsulation: ViewEncapsulation.None,
   animations: [
     trigger('state', [
       state('void', style({
