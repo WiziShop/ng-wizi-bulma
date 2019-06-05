ce143a4c0d4f07ff953a1ac8449a23d80920c21d
Jean-Baptiste Malatrasi <jb@wizishop.com>
2019-06-05 17:28:57 +0200

    fix issue

--- projects/ng-wizi-bulma/src/lib/date-picker/date-picker-module.ts
+++ projects/ng-wizi-bulma/src/lib/date-picker/date-picker-module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NwbCommonModule } from '../shared';
+import { NwbCommonModule } from '../shared/common-module';
 import { NwbDatePickerComponent } from './date-picker.component';
 import { NwbDatePickerInputStartDirective } from './date-picker-input-start.directive';
 import { NwbDatePickerInputEndDirective } from './date-picker-input-end.directive';
